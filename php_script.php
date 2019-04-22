73b5e9ec40907ab8c8d75a3adbf212341a164dca
viktor <an.viktory@gmail.com>
2019-04-22 21:21:33 +0200

    jfsljflsjdfljsdlfjs

--- php_script.php
+++ php_script.php
@@ -12,21 +12,6 @@ $input = new Input($argv);
 
 $dispatcher = new Dispatcher($input);
 
-class My extends Thread {
-    public function run() {
-        $this->synchronized(function($thread){
-            if (!$thread->done)
-                $thread->wait();
-        }, $this);
-    }
-}
-$my = new My();
 
-print_r($my);
-$my->start();
-var_dump($my->isRunning());
-$my->synchronized(function($thread){
-    $thread->done = true;
-    $thread->notify();
-}, $my);
+var_dump(sem_get(3));
 
